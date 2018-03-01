ba2f4e4ec50df705819ec749159a4015b24ba6a8
davinlondon@gmail.com <davinlondon@gmail.com>
2018-03-01 07:13:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+function happyHolidaysTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday,days) {
